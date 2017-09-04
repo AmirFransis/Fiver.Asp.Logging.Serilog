727508cc2627108cabef8e023349faea2b1596c6
Tahir Naushasd <t.naushad25@gmail.com>
2017-09-04 23:24:10 +0100

    removed the extra package and moved Log.Logger outside BuildWebHost()

--- Fiver.Asp.Logging.Serilog/Program.cs
+++ Fiver.Asp.Logging.Serilog/Program.cs
@@ -9,40 +9,33 @@ namespace Fiver.Asp.Logging.Serilog
     {
         public static void Main(string[] args)
         {
+            //Log.Logger = new LoggerConfiguration()
+            //            .WriteTo.LiterateConsole()
+            //            .CreateLogger();
+
+            //Log.Logger = new LoggerConfiguration()
+            //            .Enrich.WithProperty("ApiVersion", "1.2.5000")
+            //            .WriteTo.LiterateConsole()
+            //            .CreateLogger();
+
+            //Log.Logger = new LoggerConfiguration()
+            //            .Enrich.WithProperty("ApiVersion", "1.2.5000")
+            //            .WriteTo.LiterateConsole()
+            //            .CreateLogger()
+            //            .ForContext<HelloLoggingMiddleware>();
+
+            Log.Logger = new LoggerConfiguration()
+                        .Enrich.WithProperty("ApiVersion", "1.2.5000")
+                        .WriteTo.LiterateConsole()
+                        .WriteTo.Seq("http://localhost:5341")
+                        .CreateLogger()
+                        .ForContext<HelloLoggingMiddleware>();
+
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .ConfigureLogging((context, builder) =>
-                {
-                    builder.ClearProviders(); // to remove default (console, debug) providers
-
-                    builder.AddFilter((category, level) =>
-                        category.Contains("Fiver.Asp.Logging.Serilog.HelloLoggingMiddleware"));
-
-                    //Log.Logger = new LoggerConfiguration()
-                    //            .WriteTo.LiterateConsole()
-                    //            .CreateLogger();
-
-                    //Log.Logger = new LoggerConfiguration()
-                    //            .Enrich.WithProperty("ApiVersion", "1.2.5000")
-                    //            .WriteTo.LiterateConsole()
-                    //            .CreateLogger();
-
-                    //Log.Logger = new LoggerConfiguration()
-                    //            .Enrich.WithProperty("ApiVersion", "1.2.5000")
-                    //            .WriteTo.LiterateConsole()
-                    //            .CreateLogger()
-                    //            .ForContext<HelloLoggingMiddleware>();
-
-                    Log.Logger = new LoggerConfiguration()
-                                .Enrich.WithProperty("ApiVersion", "1.2.5000")
-                                .WriteTo.LiterateConsole()
-                                .WriteTo.Seq("http://localhost:5341")
-                                .CreateLogger()
-                                .ForContext<HelloLoggingMiddleware>();
-                })
                 .UseStartup<Startup>()
                 .UseSerilog()
                 .Build();
